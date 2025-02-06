62852f813efe8e1acacc953a936d72f05dd925b4
Andrea Aceto <andrea.aceto20@gmail.com>
2025-02-06 15:39:11 +0100

    [ADD]Milestone 1

---
+++ controllers/postController.js
@@ -0,0 +1,45 @@
+const posts = require("../data/postsData")
+
+function index(req, res) {
+    res.json(posts);
+}
+
+function show(req, res) {
+    const id = parseInt(req.params.id);
+
+    const post = posts.find(post => post.id === id);
+    if (!post) {
+
+        res.status(404);
+
+        return res.json({
+            error: "Non trovato",
+            message: "Post non trovato"
+        }
+        )
+    }
+    res.json(post);
+}
+
+function store(req, res) {
+    // const id = parseInt(req.params.id);
+    res.send("creazione nuovo post");
+}
+
+function update(req, res) {
+    res.send("modifica completa del post numero " + req.params.id)
+}
+
+function modify(req, res) {
+    res.send("modifica parziale del post numero " + req.params.id);
+}
+
+function destroy(req, res) {
+    const id = parseInt(req.params.id);
+
+    res.sendStatus(204)
+
+}
+
+
+module.exports = { index, show, store, update, modify, destroy };
---
+++ data/postsData.js
@@ -0,0 +1,39 @@
+const post = [
+    {
+        id: 1,
+        titolo: "ciambellone",
+        contenuto: "foto di ciambellone",
+        img: "./images/ciambellone.jpeg",
+        tags: "dolci, fatto in casa, ciambellone",
+    },
+    {
+        id: 2,
+        titolo: "cracker barbabietola",
+        contenuto: "foto di cracker alla barbabietola",
+        img: "./images/cracker_barbabietola.jpeg",
+        tags: "cracker, fatto in casa",
+    },
+    {
+        id: 3,
+        titolo: "pane fritto",
+        contenuto: "foto di pane fritto dolce",
+        img: "./images/pane_fritto_dolce.jpeg",
+        tags: "dolci, pane fritto",
+    },
+    {
+        id: 4,
+        titolo: "pasta barbabietola",
+        contenuto: "foto di pasta alla barbabietola",
+        img: "./images/pasta_barbabietola.jpeg",
+        tags: "pasta",
+    },
+    {
+        id: 5,
+        titolo: "torta paesana",
+        contenuto: "foto di torta paesana",
+        img: "./images/torta_paesana.jpeg",
+        tags: "dolci, torta paesana",
+    },
+];
+
+module.exports = post;
